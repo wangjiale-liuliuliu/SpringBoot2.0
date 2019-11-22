b3119a3870836120e1a0d8002cadc6209fd6ca72
王家乐 <wang2636614841@163.com>
2019-11-22 21:10:01 +0800

    项目初始化login.1.1

---
+++ src/main/java/com/example/springboot_git/interceptor/MyIntercepor.java
@@ -0,0 +1,32 @@
+package com.example.springboot_git.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+public class MyIntercepor implements HandlerInterceptor {
+    //目标方法执行前
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Object loginUser = request.getSession().getAttribute("admin");
+        if(loginUser == null){
+            request.setAttribute("msg","未登录");
+            request.getRequestDispatcher("/login").forward(request,response);
+            return false;
+        }
+        return true;
+    }
+    //
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
