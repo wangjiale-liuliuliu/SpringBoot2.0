46be6472773cfb0cd4ec7fc1ca3915a71df62330
王家乐 <wang2636614841@163.com>
2019-11-22 20:24:54 +0800

    项目初始化login0.1

--- src/main/java/com/example/springboot_git/controller/LoginController.java
+++ src/main/java/com/example/springboot_git/controller/LoginController.java
@@ -1,4 +1,25 @@
 package com.example.springboot_git.controller;
 
+import com.example.springboot_git.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class LoginController {
+    @Autowired
+    AdminService adminService;
+    @RequestMapping("/")
+    public String login(){
+        return "login";
+    }
+
+    @RequestMapping("/login")
+    public String login2(){
+        return "login";
+    }
+    @RequestMapping("/tologin")
+    public String toLogin(){
+        return "login";
+    }
 }
